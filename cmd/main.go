fa00dbec9baa119895ecbf52de1077034e420cc4
Andrey <lukigor61@gmail.com>
2021-08-08 13:40:56 +0300

    First Server Setup on Go

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"WebApplication"
+	"WebApplication/pkg/handlers"
+	"log"
+)
+
+func main() {
+	handlers := new(handlers.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
---
+++ pkg/handlers/item.go
@@ -0,0 +1,23 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) createItems(c *gin.Context) {
+
+}
+
+func (h *Handler) getAllItems(c *gin.Context) {
+
+}
+
+func (h *Handler) getItemById(c *gin.Context) {
+
+}
+
+func (h *Handler) updateItem(c *gin.Context) {
+
+}
+
+func (h *Handler) deleteItem(c *gin.Context) {
+
+}
