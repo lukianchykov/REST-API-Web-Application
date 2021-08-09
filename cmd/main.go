a478c1d79236414c5cd2beea5a8d63cb21bfc0c5
Andrey <lukigor61@gmail.com>
2021-08-09 17:12:25 +0300

    Database Migrating: Using PostgreSql, Docker. Added 2 sql schemes, config for Docker configuration

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"WebApplication"
-	"WebApplication/pkg/handlers"
+	"WebApplication/pkg/handler"
+	"WebApplication/pkg/repository"
+	"WebApplication/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	handlers := new(handlers.Handler)
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+
 }
